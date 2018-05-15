1e8d18d6dfce051dfd39e4bfbb2fa081f60e57c1
Smriti Mittal <mittal.smriti001@gmail.com>
2018-05-15 10:58:07 +0530

    instance.uer to instance

--- marketing/models.py
+++ marketing/models.py
@@ -50,6 +50,6 @@ pre_save.connect(marketing_pref_update_receiver, sender=MarketingPreference)
 
 def make_marketing_pref_receiver(sender, instance, created, *args, **kwargs):
 	if created:
-		MarketingPreference.objects.get_or_create(user=instance.user)
+		MarketingPreference.objects.get_or_create(user=instance)
 
 post_save.connect(make_marketing_pref_receiver, sender=settings.AUTH_USER_MODEL)
